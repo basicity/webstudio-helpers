3975da51c14f47a32dd25a398cf34251903322c5
Frederik Duchi <frederik.duchi@telenet.be>
2023-11-01 07:18:10 +0100

    added documentation notes

--- oauth.js
+++ oauth.js
@@ -1,4 +1,9 @@
 const ws_oAuth = (function () {
+    /**
+     * Convert the code parameter in the querystring to an access token that can be used for API calls
+     * @param {string} backend_url - The url of the service that will make the call to the token provider. This needs to be done server side to prevent exposure of the client_secret
+     * @returns {object} - The entire authentication object containing the access token and additional information.
+    */
     async function get_token(backend_url) {
         // check if the querystring contains a parameter with a code
         // only execute the remainging functions if that code was found
@@ -14,6 +19,12 @@ const ws_oAuth = (function () {
         }
     }
 
+    /**
+     * Makes an API call to an endpoint and retrieves the response as JSON
+     * @param {string} endpoint_url - The entire url of the endpoint that needs to be called
+     * @param {string} token - A valid access token that is attached to the header of the request
+     * @returns {object} - The response of the API call in JSON format
+    */
     async function call_endpoint(endpoint_url, token) {
         // check strava documentation for endpoints: https://developers.strava.com/docs/reference/
         const response = await fetch(endpoint_url, {
@@ -22,7 +33,6 @@ const ws_oAuth = (function () {
             }
         })
         const data = await response.json();
-        console.log(data);
         return data;
     }
 
--- parser.js
+++ parser.js
@@ -1,5 +1,11 @@
 const ws_parser = (function () {
 
+    /**
+     * Updates the HTML content with the actual data based on the classnames of the HTML elements where each classname represents a property.
+     * This function does not returns a value, but updates the existing HTML.
+     * @param {string} container_name - The classname of the HTML parent element that needs to be updated
+     * @param {object} data - The JSON object containing the data to update the HTML
+    */
     function render(container_name, data) {
         // get all the HTML that needs to be rendered
         const container = document.querySelector(`.${container_name}`)
@@ -17,7 +23,7 @@ const ws_parser = (function () {
             const options = Array.from(list).filter(c => c.startsWith('option__'));
             console.log(options);
             options.forEach(o => {
-                content = eval(`${o}('${content}')`)
+                content = eval(`${o.split('--')[0]}('${content}','${o.split('--').length > 0 ? o.split('--')[1] : ''}')`)
             })
 
             // fill in the textContent or src
@@ -30,6 +36,11 @@ const ws_parser = (function () {
         })
     }
 
+    /**
+     * Returns a given time in seconds to the format 00h 00min 00sec
+     * @param {number} seconds - The amount of seconds that needs to be converted.
+     * @returns {string} - The converted seconds
+    */
     function option__totime(seconds) {
         const hours = Math.floor(seconds / 3600);
         const minutes = Math.floor((seconds % 3600) / 60);
@@ -37,16 +48,41 @@ const ws_parser = (function () {
         return `${hours}h ${minutes}min ${remainingSeconds}sec`
     }
 
-    function option__separator_coma(content) {
-        return content.trim().split(' ').join(' - ')
+    /**
+     * Replaces the spaces in a string with a comma
+     * @param {string} content - The string that needs to be replaced
+     * @returns {string} - The replaced string
+    */
+    function option__separator_comma(content) {
+        return content.trim().split(' ').join(', ')
     }
 
+    /**
+     * Replaces the spaces in a string with a dash
+     * @param {string} content - The string that needs to be replaced
+     * @returns {string} - The replaced string
+    */
     function option__separator_dash(content) {
         return content.trim().split(' ').join(' - ')
     }
 
+    /**
+     * Divides a number by 1000
+     * @param {number} content - The number that needs to be divided
+     * @returns {number} - The diveded number
+    */
     function option__divide_by_thousand(content) {
         return Math.round(content / 1000);
+    }
+
+    /**
+     * Adds a unit (string) at the end of the input
+     * @param {string} content - The original string
+     * @param {string} unit - The unit (string) that needs to be attached to the original string
+     * @returns {string} - The original string followed by the unit (string)
+    */
+    function option__add_unit(content, unit) {
+        return `${content} ${unit}`
     }
 
     return {
